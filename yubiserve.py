3e536e544600aded7367242e3752065d125ccdd7
b1galez@gmail.com <b1galez@gmail.com@fbcee277-3294-991b-8290-beb7048acdd6>
2013-05-16 19:18:46 +0000

    Starting to fix what doesn't allow it to execute correctly.

--- yubiserve.py
+++ yubiserve.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# apt-get install python-pycryptopp pycrypto python-pysqlite2 python-crypto 
 
 from yubiserve_modules import configparser
 from yubiserve_modules import httphandler
